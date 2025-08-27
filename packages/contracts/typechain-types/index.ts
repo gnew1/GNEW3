96e83b913e0e4c57d699258c0c1bddef141f9dd5
gnew1 <gneweuro@gmail.com>
2025-08-27 16:22:30 +0100

    refactor governance scripts to use shared contract types

---
+++ packages/contracts/typechain-types/index.ts
@@ -0,0 +1,4 @@
+export type { GnewGovToken } from "./GnewGovToken";
+export { GnewGovToken__factory } from "./GnewGovToken";
+export type { GnewGovernor } from "./GnewGovernor";
+export { GnewGovernor__factory } from "./GnewGovernor";
